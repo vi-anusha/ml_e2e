a348698d452d4db9e1a73b2d11982cc1e1502f4b
vi-anusha <anushavirri1004@gmail.com>
2026-02-12 11:26:23 +0530

    deployment configuration

--- application.py
+++ application.py
@@ -39,6 +39,6 @@ def predict_datapoint():
         return render_template('home.html', results = results[0])
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5001)
 
 
